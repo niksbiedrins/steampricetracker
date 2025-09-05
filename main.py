2384e6bd1d942f96c0df86734f939e7fa6d4dafe
Niks Biedrins <niks.biedrins@outlook.com>
2025-09-05 21:23:50 +0100

    refactor: Use Steam API instead of web scraping

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 import re
 import requests
-from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-get_app_data_response = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2/", headers=headers)
-data = get_app_data_response.json()
+
+# steam app list
+app_list = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2/", headers=headers)
+app_list_json = app_list.json()
 
 def get_gamename():
     return str(input("Enter the game name you want to track: "))
 
 def get_appid(game_name: str):
-    for app in data["applist"]["apps"]:
+    for app in app_list_json["applist"]["apps"]:
         if app["name"].lower() == game_name.lower():
             return app["appid"]
 
-def get_app_html(appid: int):
-    steam_app_response = requests.get(f"https://store.steampowered.com/app/{appid}")
-    return BeautifulSoup(steam_app_response.text, "html.parser")
+def get_price(appid, country_code: str):
+    app_data = requests.get(f"https://store.steampowered.com/api/appdetails?appids={appid}&cc={country_code}")
+    app_data_json = app_data.json()
 
-def parse_app_price(app_html):
-    price_container = app_html.find_all("div", class_="game_purchase_action")
-    
-    for element in price_container:
-        price_div =  element.find("div", class_="game_purchase_price price")
-        price_text = price_div.get_text(strip=True)
-        formatted_price = float(re.sub(r"[^\d.]", "", price_text)) # remove currency symbol and cast to a float.
-        return formatted_price
+    final_formatted = app_data_json[str(appid)]["data"]["price_overview"]["final_formatted"]
+
+    return final_formatted
 
 # main code
 gamename = get_gamename()
 appid = get_appid(gamename)
-app_html = get_app_html(appid)
-price = parse_app_price(app_html)
+price = get_price(appid, "UK")
 
 print(price)
